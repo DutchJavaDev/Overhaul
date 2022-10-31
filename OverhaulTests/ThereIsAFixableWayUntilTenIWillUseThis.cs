e259af3eb933844a7669e02d1dde38789368e30c
Boris <Borismulder@outlook.com>
2022-10-31 20:55:26 +0100

    kinda starting to understand what developer exp means

---
+++ OverhaulTests/ThereIsAFixableWayUntilTenIWillUseThis.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace OverhaulTests
+{
+    [TestClass]
+    public class ThereIsAFixableWayUntilTenIWillUseThis
+    {
+        [TestMethod]
+        public void GetConnectionStringTest()
+        {
+            // Act
+            var connectionString = TestHelper.GetString("secrete");
+
+            // Assert
+            Assert.AreEqual("found me!",connectionString);
+        }
+    }
+}
