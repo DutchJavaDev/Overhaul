b38c382bb4224aff6e0046a8705fe0f96216e3f2
Boris <Borismulder@outlook.com>
2022-11-04 22:50:41 +0100

    schema create and update checks

--- Overhaul/Core/ModelTracker.cs
+++ Overhaul/Core/ModelTracker.cs
@@ -11,7 +11,7 @@ namespace Overhaul.Core
     internal static class ModelTracker
     {
         private static readonly string _tablePrefix = "tbl";
-        private static IEnumerable<TableDef> _cache;
+        private static IEnumerable<TableDefinition> _cache;
         private static ISqlGenerator sqlGenerator;
         private static ISqlModifier sqlModifier;
 
@@ -20,58 +20,101 @@ namespace Overhaul.Core
             // If debug read connectionstring from secrets?
             sqlGenerator = new SqlGenerator(connectionString);
 
+            // Create definitions
+            var definitions = CreateDefinitions(types);
+            
             // Check Db
             _cache = LoadCache();
 
-            // Create definitions
-            var definitions = CreateDefinitions(types);
 
-            if (_cache.Any() && definitions.Any())
+            if (definitions.Any())
             {
-                // Run definition compare
-                    // Add column
-                    // Delete column
-                // Update / Delete
-                // Done
+                RunSchemaCreate(definitions.Where(i => 
+                !_cache.Any(ii => ii.DefType == i.DefType)));
+
+                // DefType are the same but columns don't match up
+                RunSchemaUpdate(definitions.Where(i
+                    => _cache.Any(ii => ii.DefType == i.DefType && !i.Equals(ii))));
             }
         }
 
-        internal static IEnumerable<TableDef> LoadCache()
+        internal static void RunSchemaCreate(IEnumerable<TableDefinition> addedTables)
         {
-            var def = BuildDef(typeof(TableDef));
+            foreach(var table in addedTables)
+            {
+                sqlGenerator.CreateTable(table);
+            }
+        }
+
+        internal static void RunSchemaUpdate(IEnumerable<TableDefinition> modifiedTabes)
+        {
+            foreach (var table in modifiedTabes)
+            {
+                var oldType = _cache.Where(i => i.DefType == table.DefType).First();
+
+                var oldTypeColumnArray = Supported.ConvertTypesStringToArray(oldType.ColumnCollection);
+
+                var newTypeColumnArray = Supported.ConvertTypesStringToArray(table.ColumnCollection);
+
+                var addedColumns = Supported.GetAddedColumns(oldTypeColumnArray, newTypeColumnArray);
+
+                var deletedColumns = Supported.GetDeletedColumns(oldTypeColumnArray, newTypeColumnArray);
+
+                foreach(var column in addedColumns)
+                {
+                    sqlModifier.AddColumn(table.TableName, column);
+                }
+
+                foreach(var column in deletedColumns)
+                {
+                    sqlModifier.DeleteColumn(table.TableName, column);
+                }
+            }
+        }
+
+
+        internal static IEnumerable<TableDefinition> LoadCache()
+        {
+            var def = BuildDef(typeof(TableDefinition));
 
             if (!sqlGenerator.TableExists(def.TableName))
             {
                 sqlGenerator.CreateTable(def);
 
-                return Enumerable.Empty<TableDef>();
+
+                // Don't return def since it has just been created
+                // Otherwise it will trigger a create table ... again
+                return Enumerable.Empty<TableDefinition>();
             }
 
             return sqlGenerator.GetCollection();
         }
 
-        internal static IEnumerable<TableDef> 
+        // Move to class
+        internal static IEnumerable<TableDefinition> 
             CreateDefinitions(IEnumerable<Type> types)
         {
             return types.Select(i => BuildDef(i));
         }
 
-        private static TableDef BuildDef(Type type)
+        // Move to class
+        private static TableDefinition BuildDef(Type type)
         {
             var tableName = GetTableName(type);
             var properties = Supported.GetPropertiesForType(type);
             var columnCollection = 
                 Supported.ConvertPropertiesToTypesString(properties, out var count);
 
-            return new TableDef 
+            return new TableDefinition 
             {
                 TableName = tableName,
                 ColumnCollection = columnCollection,
                 DefType = type.Name,
-                ColumnCount = count
+                ColumnCount = count,
             };
         }
 
+        // Move to class
         internal static string GetTableName(Type type)
         {
             if (Attribute.IsDefined(type, typeof(TableAttribute))
