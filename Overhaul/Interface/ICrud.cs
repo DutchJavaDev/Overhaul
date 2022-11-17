96b39a7096280bf9e4378b50c0fe597d0ea00ea4
Boris <Borismulder@outlook.com>
2022-11-17 18:52:21 +0100

    docs

--- Overhaul/Interface/ICrud.cs
+++ Overhaul/Interface/ICrud.cs
@@ -24,9 +24,34 @@
         /// <param name="entity"></param>
         /// <returns></returns>
         bool Update<T>(T entity) where T : class;
+        /// <summary>
+        /// Gets a entity by its primary key/id
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
         T GetById<T>(object id) where T : class;
+        /// <summary>
+        /// Gets an entity by a specific column field
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="columnName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         T GetBy<T>(string columnName, object value) where T : class;
+        /// <summary>
+        /// Gets a collection of entity's
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         IEnumerable<T> GetCollection<T>() where T : class;
+        /// <summary>
+        /// Gets s collection of entity's based of a column value 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="columnName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         IEnumerable<T> GetCollectionWhere<T>(string columnName, object value) where T : class;
         /// <summary>
         /// Delete an existing entity
